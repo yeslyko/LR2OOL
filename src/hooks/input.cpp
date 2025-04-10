98b6cbdaaf359b109850128e6121699b7f6ed1ea
Tenai <tenai@tutanota.com>
2025-04-10 12:37:43 -0700

    calling get_mouse_input seems to be important to do (seems to only affect scaled clients)

--- src/hooks/input.cpp
+++ src/hooks/input.cpp
@@ -3,9 +3,11 @@
 
 uint8_t input::GetMouseInput()
 {
+	uint8_t result = get_mouse_input_hook.call<uint8_t>();
+
 	if (overlay::open)
 		return 0;
-	else return get_mouse_input_hook.call<uint8_t>();
+	else return result;
 }
 
 void input::Install()
